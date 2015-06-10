d10abd53343ad2b31725f4b428ad6a279aa97746
Rahul Kumar <kumarah@gmail.com>
2015-06-10 14:21:10 -0700

    Added document updates, updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pymongotut
 A PyMongo tutorial with MongoDB running on a Raspberry Pi 2
+
+Raspberry Pi 2 Setup:
+
+1. Enable Static IP
+2. Enable SSH from raspi-config tool
+3. Update and upgrade
+    * sudo apt-get update
+    * sudo apt-get upgrade
+
+For installing MongoDB on a Raspberry Pi 2, we need to use a special port available on Github (thanks to skrabban):
+
+[skrabban/mongo-nonx86](https://github.com/skrabban/mongo-nonx86)
+
+To install MongoDB on the Rapberry Pi 2, follow the instruction by Hardware Hacks :
+
+[Raspberry Pi MongoDB Installation - The working guide!](http://c-mobberley.com/wordpress/2013/10/14/raspberry-pi-mongodb-installation-the-working-guide/)
--- pymongotut.py
+++ pymongotut.py
@@ -196,6 +196,18 @@ for document in cursor:
 
 
 '''
+Find indian restaurants with grade scores less than 10  or greater than 30 in zipcode 10075 and 11369
+'''
+print '------------------------------------------------------------'
+print 'Indian restaurants in 10075 or 11369 with scores <10 and >30'
+print '------------------------------------------------------------'
+cursor = db.restaurants.find({"cuisine":"Indian","address.zipcode":{"$in":["10075","11369","11216"]},"$or":[{"grades.score":{"$lt":10}},{"grades.score":{"$gt":30}}]})
+print '---------------'
+print 'Indian restaurants:'+str(cursor)
+for document in cursor:
+    print document
+
+'''
 Find indian restaurants with grade scores less than 10  or greater than 30 in zipcode 10075, sorted by score
 '''
 cursor = db.restaurants.find({"cuisine":"Indian","$or":[{"grades.score":{"$lt":10}},{"grades.score":{"$gt":30}}]}).sort("grades.score",pymongo.ASCENDING)
@@ -204,3 +216,95 @@ print 'Indian restaurants:'+str(cursor)
 for document in cursor:
     print document
 
+
+'''
+-------------------------------------------------------------------------------
+Updating Data/Documents
+-------------------------------------------------------------------------------
+'''
+'''
+Updating a top level field
+'''
+
+result = db.restaurants.update_one({"name":"Juni"},{"$set":{"cuisine":"American (Old)"}})
+
+print 'How many results matched the search criteria: ' + str(result.matched_count)
+print 'How many documents were updated: ' + str(result.modified_count)
+
+cursor = db.restaurants.find({"name":"Juni"})
+for document in cursor:
+    print document
+
+
+'''
+Updating an embedded field
+'''
+
+cursor = db.restaurants.find({"restaurant_id":"41156888"})
+for document in cursor:
+    print document
+
+
+print 'Updating restaurant address'
+result = db.restaurants.update_one({"restaurant_id":"41156888"},{"$set":{"address.street":"East 31st Street"}})
+print 'How many results matched the search criteria: ' + str(result.matched_count)
+print 'How many documents were updated: ' + str(result.modified_count)
+
+
+cursor = db.restaurants.find({"restaurant_id":"41156888"})
+for document in cursor:
+    print document
+
+
+'''
+Updating multiple documents
+'''
+print 'Updating multiple documents'
+
+result = db.restaurants.update_many(
+    {"address.zipcode": "10016", "cuisine": "Other"},
+    {
+        "$set": {"cuisine": "Category To Be Determined"}
+    }
+)
+
+print 'How many results matched the search criteria: ' + str(result.matched_count)
+print 'How many documents were updated: ' + str(result.modified_count)
+
+
+cursor = db.restaurants.find({"address.zipcode": "10016", "cuisine": "Category To Be Determined"})
+for document in cursor:
+    print document
+
+
+'''
+Replacing a document
+'''
+print '----------------------------------------------'
+print 'Replacing a document'
+print 'Find the first document matching criteria and modify the document schema'
+cursor = db.restaurants.find_one({"restaurant_id": "41704620"})
+print cursor
+
+
+
+result = db.restaurants.replace_one(
+    {"_id": cursor['_id']},
+    {
+        "name": "Vella 2",
+        "address": {
+            "coord": [-73.9557413, 40.7720266],
+            "building": "1480",
+            "street": "2 Avenue",
+            "zipcode": "10075"
+        },
+        "restaurant_id":"41704620"
+    }
+)
+
+print 'How many results matched the search criteria: ' + str(result.matched_count)
+print 'How many documents were updated: ' + str(result.modified_count)
+
+cursor = db.restaurants.find({"restaurant_id": "41704620"})
+for document in cursor:
+    print document
